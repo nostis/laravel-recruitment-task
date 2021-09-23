76fae9ecbd835e5d344dae5e56b81de7b12bdf72
Norbert <n.szumera@cogitech.pl>
2021-09-23 18:34:18 +0200

    Add simple blade template

---
+++ resources/views/page.blade.php
@@ -0,0 +1,29 @@
+<head>
+    <title>Page</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+
+<h2>First section</h2>
+
+<label>
+    <input type="number" id="input_1">
+</label>
+
+<label>
+    <input type="number" id="input_2">
+</label>
+
+<button id="button">Count</button>
+
+<p id="result"></p>
+
+<hr>
+
+<h2>Second section</h2>
+<p>Second section</p>
+
+<script>
+    $(function() {
+        console.log( "ready!" );
+    });
+</script>
